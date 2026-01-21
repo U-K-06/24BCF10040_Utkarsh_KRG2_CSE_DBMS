ebd5b0c19708f12fd2d283e96ca6b59cc2595af2
Utkarsh Khajuria <168301041+U-K-06@users.noreply.github.com>
2026-01-21 20:39:31 +0530

    Update experiment2.sql

--- Experiment_2/experiment2.sql
+++ Experiment_2/experiment2.sql
@@ -6,14 +6,42 @@ CREATE TABLE employee (
     joining_date DATE
 );
 
-INSERT INTO employee VALUES (1, 'Amit', 'HR', 25000, '2020-01-10');
-INSERT INTO employee VALUES (2, 'Neha', 'HR', 35000, '2019-03-15');
-INSERT INTO employee VALUES (3, 'Rahul', 'IT', 40000, '2018-07-22');
-INSERT INTO employee VALUES (4, 'Sneha', 'IT', 15000, '2021-02-11');
-INSERT INTO employee VALUES (5, 'Vikas', 'Finance', 50000, '2017-11-05');
-INSERT INTO employee VALUES (6, 'Anita', 'Finance', 28000, '2020-06-18');
-INSERT INTO employee VALUES (7, 'Rohit', 'Sales', 22000, '2019-09-30');
-INSERT INTO employee VALUES (8, 'Pooja', 'Sales', 32000, '2018-12-12');
+INSERT INTO employee VALUES (1, 'Amit', 'HR', 25000, DATE '2020-01-10');
+INSERT INTO employee VALUES (2, 'Neha', 'HR', 35000, DATE '2019-03-15');
+INSERT INTO employee VALUES (3, 'Rahul', 'IT', 40000, DATE '2018-07-22');
+INSERT INTO employee VALUES (4, 'Sneha', 'IT', 15000, DATE '2021-02-11');
+INSERT INTO employee VALUES (5, 'Vikas', 'Finance', 50000, DATE '2017-11-05');
+INSERT INTO employee VALUES (6, 'Anita', 'Finance', 28000, DATE '2020-06-18');
+INSERT INTO employee VALUES (7, 'Rohit', 'Sales', 22000, DATE '2019-09-30');
+INSERT INTO employee VALUES (8, 'Pooja', 'Sales', 32000, DATE '2018-12-12');
+
+SELECT * 
+FROM employee;
+
+SELECT emp_name, salary 
+FROM employee
+WHERE salary > 20000
+ORDER BY salary DESC;
+
+SELECT department, COUNT(*) AS total_employees
+FROM employee
+GROUP BY department;
+
+SELECT department, SUM(salary) AS total_salary
+FROM employee
+GROUP BY department;
+
+SELECT department, MIN(salary) AS minimum_salary
+FROM employee
+GROUP BY department;
+
+SELECT department, MAX(salary) AS maximum_salary
+FROM employee
+GROUP BY department;
+
+SELECT department, AVG(salary) AS average_salary
+FROM employee
+GROUP BY department;
 
 SELECT department, AVG(salary) AS average_salary
 FROM employee
